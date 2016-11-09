5e9c20cafc38a5ec2a577f272b50b1b3a0844f5e
TitouanT <titouan.teyssier@gmail.com>
2016-11-09 16:36:39 +0100

    fin seance

--- global.h
+++ global.h
@@ -1,8 +1,10 @@
 #define TRUE 1
 #define FALSE 0
+#define LINES 20
+#define COLUMNS 90
 
 typedef enum {dirUP, dirDOWN, dirLEFT, dirRIGHT} t_dir;
-typedef enum {fxEMPTY, fxWALL, fxROOM, fxCORRIDOR, fxDOOR} t_fixe;
+typedef enum {fxEMPTY, fxWALL, fxROOM, fxCORRIDOR, fxDOOR, fxPORTEOUVERTE, fxPORTEFERMEE, fxESCALIERM, fxESCALIERD} t_fixe;
 typedef enum {wV, wH, wUR, wUL, wDR, wDL} t_wall;
 typedef enum {dNONE, dOPEN, dCLOSE} t_door;
 typedef enum {int line, column, lvl} t_pos;
