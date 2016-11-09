f36351dd32ae2526075b0fbd68013ed98ba351ca
TitouanT <titouan.teyssier@gmail.com>
2016-11-09 15:59:18 +0100

    update type.h

--- type.h
+++ type.h
@@ -1,3 +1,7 @@
+#define TRUE 1
+#define FALSE 0
+
+
 typedef enum {fxEMPTY, fxWALL, fxROOM, fxCORRIDOR, fxDOOR} t_fixe;
 typedef enum {wV, wH, wUR, wUL, wDR, wDL} t_wall;
 typedef enum {dNONE, dOPEN, dCLOSE} t_door;
@@ -7,5 +11,3 @@ typedef struct {
 	t_wall w;
 	t_door d;
 } t_cell;
-#define TRUE 1
-#define FALSE 0
