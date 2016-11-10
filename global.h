deb631adc4fc4d18c3efa6fced4924515d92838c
TitouanT <titouan.teyssier@gmail.com>
2016-11-10 17:27:52 +0100

    securisation de global.h

--- global.h
+++ global.h
@@ -1,15 +1,14 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
 #define TRUE 1
 #define FALSE 0
 #define LINES 20
 #define COLUMNS 90
 
-typedef enum {dirUP, dirDOWN, dirLEFT, dirRIGHT} t_dir;
-typedef enum {fxEMPTY, fxWALL, fxROOM, fxCORRIDOR, fxDOOR, fxPORTEOUVERTE, fxPORTEFERMEE, fxESCALIERM, fxESCALIERD} t_fixe;
-typedef enum {wV, wH, wUR, wUL, wDR, wDL} t_wall;
-typedef enum {dNONE, dOPEN, dCLOSE} t_door;
+#include "cell.h"
+
 typedef enum {int line, column, lvl} t_pos;
-typedef struct {
-	t_fixe f;
-	t_wall w;
-	t_door d;
-} t_cell;
+typedef enum {dirUP, dirDOWN, dirLEFT, dirRIGHT} t_dir;
+
+#endif /* end of include guard: GLOBAL_H */
