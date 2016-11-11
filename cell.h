bc875233831dc98d68e27c091111e6e57f2232f9
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-11 14:28:04 +0100

    update cell.h

--- cell.h
+++ cell.h
@@ -1,17 +1,26 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include "bool.h"
+
 typedef enum {EMPTY, WALL, ROOM, CORRIDOR, DOOR} t_type;
+
+typedef struct {
+	t_type type; // ROOM, CORRIDOR
+	t_bool discovered;
+} t_floor;
 
 typedef enum {wV, wH, wUR, wUL, wDR, wDL} t_wallType;
 typedef struct {
 	t_type type; // WALL
+	t_bool discovered;
 	t_type wallType; //
 } t_wall;
 
 typedef enum {dNONE, dOPEN, dCLOSE} t_doorStates;
 typedef struct {
 	t_type type; // DOOR
+	t_bool discovered;
 	t_doorStates doorStates; // dNONE, dOPEN or dCLOSE
 } t_door;
 
@@ -19,11 +28,12 @@ typedef struct {
 typedef union {
 	t_type type; /* Commun a tous les type*/
 
+	t_floor walk; // en attente d'un meilleur nom (floor est pris par le C)
 	t_wall wall;
 	t_door door;
 } t_cell;
 
-/* definition type d'un nouveau cham de l'union
+/* definition type d'un nouveau champ de l'union
 typedef struct {
 	t_type type; //
 } t_;
