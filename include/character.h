fa16cb3bb9aec8c9dd84016c8809fe630645403e
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-11 22:20:06 +0100

    update character

--- include/character.h
+++ include/character.h
@@ -1,7 +1,17 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-typedef struct {int line, column, lvl;} t_pos;
+
 typedef enum {UP, DOWN, LEFT, RIGHT} t_dir;
+
+typedef struct {
+	char name[100];
+	int line;
+	int column;
+	int lvl;
+	int hp;
+	int pw;
+	int xp;
+} t_character;
 
 #endif /* end of include guard: CHARACTER_H */
