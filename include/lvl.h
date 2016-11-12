db8eaeaa08894e6774e06e7386534991561a5bec
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-12 16:56:53 +0100

    add readFloor and writeFloor

--- include/lvl.h
+++ include/lvl.h
@@ -5,6 +5,29 @@
 #include "mapConst.h"
 
 void randomFloor (t_cell map[LINES][COLUMNS], int step);
+int readLvl (char * fileName, t_cell map[][COLUMNS]);
+void writeLvl (char * fileName, t_cell map[][COLUMNS]);
+
+/*
+
+on a besoin de :
+	createWorld
+	loadNextlvl
+	loadPrevlvl
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 //void displayFloor (t_cell map[][COLUMNS]);
 
 #endif /* end of include guard: LVL_H */
