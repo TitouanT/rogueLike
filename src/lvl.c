1a6fb2e4c039be2a2af58ba2fd7ee74451580b1d
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-12 18:57:01 +0100

    add comment in lvl.c

--- src/lvl.c
+++ src/lvl.c
@@ -113,20 +113,20 @@ int randab (int a, int b) { // return a number in [a,b[
 	return (rand()%(b-a)) + a;
 }
 
-int isBetween (int val, int a, int b) {
-	if (a <= val && b >= val) return 1;
-	else return 0;
+int isBetween (int val, int a, int b) { // return TRUE if val is in [a,b]
+	if (a <= val && b >= val) return TRUE;
+	else return FALSE;
 }
 
-int intervalOverlaping (int a, int b, int c, int d) {
-	if (isBetween (a, c, d) || isBetween (b, c, d) || isBetween (c, a, b) || isBetween (d, a, b)) return 1;
-	else return 0;
+int intervalOverlaping (int a, int b, int c, int d) { // return TRUE if the intersection of [a,b] and [c,d] is not empty
+	if (isBetween (a, c, d) || isBetween (b, c, d) || isBetween (c, a, b) || isBetween (d, a, b)) return TRUE;
+	else return FALSE;
 }
 
-int areInContact (t_room r1, t_room r2) {
+int areInContact (t_room r1, t_room r2) { // return TRUE if the room are in Contact
 	if (intervalOverlaping(r2.line, r2.line + r2.height, r1.line, r1.line + r1.height)
-	&& intervalOverlaping(r2.column, r2.column + r2.width, r1.column, r1.column + r1.width)) return 1;
-	else return 0;
+	&& intervalOverlaping(r2.column, r2.column + r2.width, r1.column, r1.column + r1.width)) return TRUE;
+	else return FALSE;
 }
 
 t_room randomRoom (t_cell map[][COLUMNS], t_room * rooms, int nbRoom, int *nbTotal) {
@@ -134,32 +134,31 @@ t_room randomRoom (t_cell map[][COLUMNS], t_room * rooms, int nbRoom, int *nbTot
 	t_room room;
 	acc = 0;
 	do {
+		// choose a random position for the top left corner of the room
 		room.line = randab(1, LINES - ROOM_MIN_LEN - 1);
 		room.column = randab(1, COLUMNS - ROOM_MIN_LEN - 1);
 
+		// the door dimensions are choose between the min length and the maximum they can be without going out of the map
 		maxHeight = MIN(LINES - room.line - 1, ROOM_MAX_HEIGHT);
 		room.height = randab(ROOM_MIN_LEN, maxHeight + 1);
 
 		maxWidth = MIN(COLUMNS - room.column - 1, ROOM_MAX_WIDTH);
 		room.width  = randab(ROOM_MIN_LEN, maxWidth + 1);
-		if (room.width == 0 || room.height == 0) {
-			printf("r.width: %d, r.height %d, maxWidth %d, maxHeight %d\n\n", room.width, room.height, maxWidth, maxHeight);
-			scanf("%*c");
-		}
 
+		// if the room is touching another one then try another
 		posNotOk = 0;
-		if (map[room.line][room.column].type != EMPTY) posNotOk = 1;
-		else {
-			for (i = 0; i < nbRoom && posNotOk == 0; i++) {
-				if (areInContact(room, rooms[i])) posNotOk = 1;
-			}
-		}
+		for (i = 0; i < nbRoom && posNotOk == 0; i++)
+			if (areInContact(room, rooms[i])) posNotOk = 1;
+
 		acc++;
-	} while (posNotOk && acc < maxTour);
-	if (acc == maxTour) {
+	} while (posNotOk && acc < maxTour); // maxTour is there to protect against infinite loop
+
+	if (acc >= maxTour) { // if acc reach maxTour then give up one room
 		*nbTotal = nbRoom;
 		return room;
 	}
+
+	// are you really reading that ?
 	for (i = 0; i < room.height; i++) for (j = 0; j < room.width; j++) {
 
 		if (i == 0 || i == room.height - 1 || j == 0 || j == room.width - 1) {
@@ -189,6 +188,7 @@ int minDist (t_room r1, t_room r2) { // calcul la distance entre le centre de de
 	return line + col;
 }
 
+// return the linear translation of value from [min, max] to [destMin, destMax]
 int mapping (int value, int min, int max, int destMin, int destMax) {
 	if (destMin > destMax) return destMax + destMin - mapping (value, min, max, destMax, destMin);
 	return ((destMax - destMin) * (value - min) / (max - min)) + destMin;
@@ -204,14 +204,14 @@ t_pos chooseRandomWall (t_room r) {
 	int isLeftOrTop = rand()%2;
 	t_pos rep;
 	if (isHorizontal) {
-		if (isLeftOrTop) rep.line = r.line;
-		else rep.line = r.line + r.height - 1;
-		rep.column = r.column + randab(1, r.width - 1);
+		if (isLeftOrTop) rep.line = r.line; // case horizontal top
+		else rep.line = r.line + r.height - 1; // case horizontal bottom
+		rep.column = r.column + randab(1, r.width - 1); // we don't want a angle
 	}
 	else {
-		if (isLeftOrTop) rep.column = r.column;
-		else rep.column = r.column + r.width - 1;
-		rep.line = r.line + randab(1, r.height - 1);
+		if (isLeftOrTop) rep.column = r.column; // case vertical left
+		else rep.column = r.column + r.width - 1; // case vertical right
+		rep.line = r.line + randab(1, r.height - 1); // here neither
 	}
 	return rep;
 }
@@ -221,16 +221,33 @@ void avoidTouchingDoors (t_cell map[][COLUMNS], t_pos * pos) {
 		if (map[pos->line - 1][pos->column].type != DOOR) {
 			if (map[pos->line][pos->column + 1].type != DOOR) {
 				if (map[pos->line][pos->column - 1].type != DOOR) {
+					// if all around you there are no doors, then you can be one!
 					map[pos->line][pos->column].type = DOOR;
 					map[pos->line][pos->column].door.doorStates = dCLOSE;
 					map[pos->line][pos->column].door.isDiscovered = TRUE;
 				}
-				else (pos->column)--;
+				else (pos->column)--; // else you have to be in the shaddow of one surrounding you
 			} else (pos->column)++;
 		} else (pos->line)--;
 	} else (pos->line)++;
 }
 
+/* 	createLink use an algorithm call flood and fill to search a path from a door to another,
+		the principle is:
+			1- write a zero on the starting point
+			2- add it to the queue
+			3- take a position in the queue and jump on it
+			4- if you are not at the target position then
+				4.1- read the value you are steping on
+				4.2- add 1 to it
+				4.3- for all the cell around you who (have not been visited AND can be), do
+					4.3.1- write the new value on it
+					4.3.2- add it to the queue
+				4.4- go to step 3
+			5. it's finish !!
+
+		After it start from the end and move back to the start following the decreasing value.
+*/
 void createLink (t_cell map[][COLUMNS], t_room r1, t_room r2) {
 	int path[LINES][COLUMNS];
 	int i, j, val, c, l;
@@ -271,6 +288,9 @@ void createLink (t_cell map[][COLUMNS], t_room r1, t_room r2) {
 		}
 		file_retirer(&head);
 	}
+	file_supprimer();
+
+
 	while (head.line != start.line || head.column != start.column) {
 		l = head.line;
 		c = head.column;
@@ -312,11 +332,11 @@ void createLink (t_cell map[][COLUMNS], t_room r1, t_room r2) {
 			head.column = c - 1;
 		}
 	}
-	file_supprimer();
+
 }
 
 void chooseLink (t_cell map[LINES][COLUMNS], t_room * rooms, int nbRoom) {
-	int adj[ROOM_NB_MAX][ROOM_NB_MAX] = {0};
+	int adj[ROOM_NB_MAX][ROOM_NB_MAX] = {{0}};
 	int i, j;
 	for (i = 0; i < nbRoom-1; i++) adj[i][i+1] = 1;
 	for (i = 0; i < nbRoom - 1; i++) {
@@ -355,6 +375,7 @@ void randomFloor (t_cell map[LINES][COLUMNS], int step) {
 		}
 	}
 	chooseLink (map, rooms, nbRoom);
+	placeObject (map, rooms, nbRoom);
 	// for (i = 0; i < nbRoom - 1; i++) { // la proba que deux pieces soient connecté est inversement proportionnelle a la distance qui les sépare
 	// 	for (j = i+1; j < nbRoom; j++) {
 	// 		rooms[i].link[j] = rooms[j].link[i]= minDist(rooms[i], rooms[j]);
@@ -414,5 +435,5 @@ void randomFloor (t_cell map[LINES][COLUMNS], int step) {
 	// 	}
 	// }
 
-	placeObject (map, rooms, nbRoom);
+
 }
