9e7378b3622031e3bb84e9aec007def5b15f63a3
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-15 23:54:34 +0100

    reparation d'un merge

--- src/interactions.c
+++ src/interactions.c
@@ -105,7 +105,7 @@ void traiterEntree(t_cell map[LINES][COLUMNS], t_character *player, WINDOW *win,
           writeLvl(map,(player->lvl));
           (player->lvl)++;
           readLvl(map,(player->lvl));
-          move2spawn(map, player);
+          move2spawn(map, player, STAIRS_DOWN);
         }
         else {
           addLog("Vous êtes déjà au niveau le plus haut !", lineLog, win);
@@ -117,7 +117,7 @@ void traiterEntree(t_cell map[LINES][COLUMNS], t_character *player, WINDOW *win,
           writeLvl(map,(player->lvl));
           (player->lvl)-- ;
           readLvl(map,(player->lvl));
-          move2Stairs_UP(map, player);
+          move2spawn(map, player, STAIRS_UP);
         }
         else {
           addLog("Vous êtes déjà en bas !", lineLog, win);
