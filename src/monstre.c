44cb2ba9d94af5a5a56b7dff13b727681acf81f3
TitouanT <titouan.teyssier@gmail.com>
2016-11-17 12:07:20 +0100

    try to understand the bug of atom

--- src/monstre.c
+++ src/monstre.c
@@ -14,12 +14,15 @@ void initMonster () {
 								randab(NB_LVL - 3, NB_LVL), 100, 1, 1, 1, 0, 0};
 
 	for (i = 2; i < nbMonster; i++) {
+		// mm c
+		monster[i].lvl = didItHappen();
 		monster[i].type = randab (L1, GHOST);
-		monster[]
+		monster[i].line = -1;
+		monster[i].col  = -1;
 		switch (monster[i].type) {
 			case L1: {
-
-			}
+				monster[i].lvl = didItHappen();
+			} break;
 			case L2:
 			case L3:
 			case MASTER:
