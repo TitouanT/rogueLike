331416843e3e65841808154796fb13e1605c6378
TitouanT <titouan.teyssier@gmail.com>
2016-11-26 16:45:13 +0100

    ajout de fonction dans notre boite a outils

--- include/tools.h
+++ include/tools.h
@@ -7,5 +7,8 @@
 void initRandom (void);
 int randab (int a, int b);
 int didItHappen (int chance);
+int car2val (char car);
+char up2low (char car);
+char low2up (char car);
 
 #endif /* end of include guard: RANDOM_H */
