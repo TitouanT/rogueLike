43e38db072c88feb9cf1832bb0f36524781b3dcc
Valentin Pelloin <valentinpelloin@gmail.com>
2016-12-15 11:36:32 +0100

    test son

--- src/echo_char.c
+++ src/echo_char.c
@@ -1,19 +1,24 @@
 #include <ncurses.h>
-#include <locale.h> 
+#include <locale.h>
+#include <sys/ioctl.h>
+#include <linux/kd.h>
 
 int main(){
   setlocale(LC_CTYPE, "");
-  initscr();
-  raw();
-  noecho();
-  keypad(stdscr, TRUE);
+  //initscr();
+  //raw();
+  //noecho();
+  //keypad(stdscr, TRUE);
 
   int ch;
 
-  ch = getch();
-  endwin();
-
-  printf("Keycode: %i\n",ch);
+  //ch = getch();
+  //endwin();
+  int ms = 5000;
+int freq = 440;
+int fd = 0;
+ioctl(fd, KDMKTONE, (ms<<16 | 1193180/freq));
+  //printf("Keycode: %i\n",ch);
 
   return 0;
 }
